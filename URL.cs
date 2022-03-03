2d6c0caf10d2b86b0cf590f1c93897de5627ef80
oscar chanax <oscar2001chanax@hotmail.com>
2022-03-02 21:43:03 -0600

    Creacion de clase url

---
+++ URL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navegador
+{
+    internal class URL
+    {
+        public string Url { get; set; }
+        public string veces { get; set; }
+        public string FechaAcceso { get; set; }
+
+    }
+}
